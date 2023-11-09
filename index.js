785ed0f1fbbed2bde7fa5ecabd6ba31459b28e7a
nikitanevskij <bike.rental@yandex.by>
2023-11-09 10:07:41 +0300

    fix: refactor

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import express from "express";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
 import mongoose from "mongoose";
-import { validationResult } from "express-validator";
 import { registerValidation } from "./validations/auth.js";
-import UserModel from "./models/User.js";
+import checkAuth from "./utils/checkAuth.js";
+import * as UserController from "./controllers/userController.js";
 
 mongoose
-  .connect("")
+  .connect(
+    "mongodb+srv://admin:wwwwww@cluster0.put2qck.mongodb.net/blog?retryWrites=true&w=majority",
+  )
   .then(() => console.log("DB ok"))
   .catch((err) => console.log("DB error", err));
 
@@ -15,41 +15,11 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/auth/register", registerValidation, async (req, res) => {
-  try {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json(errors.array());
-    }
+app.post("/auth/register", registerValidation, UserController.register);
 
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
+app.post("/auth/login", UserController.login);
 
-    const doc = new UserModel({
-      email: req.body.email,
-      fullName: req.body.fullName,
-      avatarUrl: req.body.avatarUrl,
-      passwordHash: hash,
-    });
-    const user = await doc.save();
-
-    const token = await jwt.sign(
-      {
-        _id: user._id,
-      },
-      "secret123",
-      { expiresIn: "30d" },
-    );
-    const { passwordHash, ...userData } = user._doc;
-    res.json({ ...userData, token });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      message: "Не удалось зарегестрироваться",
-    });
-  }
-});
+app.get("/auth/me", checkAuth, UserController.getMe);
 
 app.listen(4444, (err) => {
   if (err) {
